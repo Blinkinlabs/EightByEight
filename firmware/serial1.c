4ed1f06bf384322812038dce77261349d782efab
Matt Mets <matt.mets@cibomahto.com>
2016-07-26 12:18:24 +0800

    Firmware: don't glitch ESP reset line when resetting

--- firmware/serial1.c
+++ firmware/serial1.c
@@ -92,10 +92,11 @@ void serial_begin(uint32_t divisor)
 
 	// TODO: Move these to a better location
 	// Software RTS and DTR signals
-	CORE_PIN5_CONFIG = PORT_PCR_DSE | PORT_PCR_SRE | PORT_PCR_MUX(1);
-	CORE_PIN6_CONFIG = PORT_PCR_DSE | PORT_PCR_SRE | PORT_PCR_MUX(1);
 	GPIOB_PDDR |= 0x03;
 	GPIOB_PSOR  = 0x03;
+	CORE_PIN5_CONFIG = PORT_PCR_DSE | PORT_PCR_SRE | PORT_PCR_MUX(1);
+	CORE_PIN6_CONFIG = PORT_PCR_DSE | PORT_PCR_SRE | PORT_PCR_MUX(1);
+
 
 	UART0_BDH = (divisor >> 13) & 0x1F;
 	UART0_BDL = (divisor >> 5) & 0xFF;
