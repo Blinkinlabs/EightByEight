54b1a72a605acf1fbccd9bb5b81a391d103f3839
Matt Mets <matt@blinkinlabs.com>
2016-10-21 02:15:00 -0700

    Update drivers and attempt to sign them correctly.

--- driver/driver_signer.bat
+++ driver/driver_signer.bat
@@ -8,15 +8,15 @@
 
 
 :: Path to the Windows SDK/ Driver SDK
-set inftocatpath="C:\Program Files (x86)\Windows Kits\8.0\bin\x86\"
-set toolpath="C:\Program Files (x86)\Windows Kits\8.1\bin\x86\"
+set toolpath="C:\Program Files\Windows Kits\10\bin\x86\"
 
 :: Make a .cat file for the .inf file
-%inftocatpath%inf2cat /v /driver:%CD% /os:XP_X86,Vista_X86,Vista_X64,7_X86,7_X64,8_X86,8_X64
+%toolpath%inf2cat /v /driver:%CD% /os:XP_X86,Vista_X86,Vista_X64,7_X86,7_X64,8_X86,8_X64
 
 :: Sign the .cat file, using the GlobalSign root certificate and the private certificate
 :: Note: First, add the private certificate to the Windows install!
-%toolpath%signtool sign /v /ac "GlobalSign Root CA.crt" /n "Blinkinlabs, LLC" /tr http://tsa.starfieldtech.com *.cat
+:: %toolpath%signtool sign /v /ac "GlobalSign Root CA.crt" /n "Blinkinlabs, LLC" /tr http://tsa.starfieldtech.com *.cat
+%toolpath%signtool sign /v /ac "Root-R3.crt" /n "Blinkinlabs, LLC" /tr http://tsa.starfieldtech.com *.cat
 
 :: Finally, verify that the file was created and signed correctly.
 %toolpath%signtool verify /v /pa Blinkinlabs.cat
